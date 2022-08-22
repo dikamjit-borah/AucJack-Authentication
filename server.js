4e44091a08d95ebea2298647579491576499fc46
dikamjit_borah <dikamjit.borah@superprocure.com>
2022-08-22 17:32:04 +0530

    setup basicutils

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const TAG = "server.js"
+
+const express = require('express')
+const cors = require('cors');
+
+const basicUtils = require('./utils/basic.utils');
+const constants = require('./utils/constants');
+const app = express()
+
+const port = process.env.PORT || 5050;
+var corsOptions = {
+  origin: `${process.env.HOST}:${port}`,
+};
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(port, () => {
+  basicUtils.logger(TAG, `AucJack-Authentication running on port ${port}`)
+});
+
